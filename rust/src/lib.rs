2ce90ec45e09432cb065f27a873ef3447809cde0
Dan Gorlyakov <d.gorlyakov@picodata.io>
2025-04-09 18:58:27 +0300

    feat(handler): fibreq test api call

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,15 @@ use tarantool::proc;
 
 #[proc]
 fn get_api_string() -> String {
-    let api_string = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=your_api_key";
-    api_string.to_string()
+    let api_string = "https://api.open-meteo.com/v1/forecast?latitude=55.7558&longitude=37.6176&current=temperature_2m";
+    
+    let client = fibreq::ClientBuilder::new().build();
+    let mut response = client.get(api_string).unwrap().send().unwrap();
+
+    println!("Status: {}", response.status());
+    if response.status().is_success() {
+        response.text().unwrap()
+    } else {
+        "bad request".to_string()
+    }
 }
